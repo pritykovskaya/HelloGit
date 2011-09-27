5d675caf270a4ab35bfc0565dd5c1feda7bf00df
Victor <victor.passichenko@gmail.com>
2011-09-27 18:37:47 +0400

    ,k

---
+++ src/HelloGit.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Raven
+ * Date: 27.09.11
+ * Time: 18:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+    }
+}
